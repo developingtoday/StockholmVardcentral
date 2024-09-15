1da4bcabc6c611cca93776dbf87c2732f89d20f5
stelianp <stelian.popa@reguity.com>
2024-09-15 18:20:57 +0200

    final stuff.

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import pandas as pd
 import folium
 import streamlit as st
 from streamlit_folium import st_folium
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+import io
+
+from watchdog.observers.winapi import CreateFileW
 
 st.title('Vardcentral in Stockholm')
 
@@ -15,7 +21,7 @@ st.title('Vardcentral in Stockholm')
 @st.cache_data
 def load_dynamic_data():
     base_url = "https://www.1177.se/Stockholm"
-    url = "https://www.1177.se/api/hjv/search?st=49d6e0b2-1e69-40f9-998e-490cf31722c3&nearby=false&s=distance&g=ChIJywtkGTF2X0YRZnedZ9MnDag&lat=&lng=&location=Stockholm%20kommun&caretype=V%C3%A5rdcentral&q=&p=1&componentname=&batchsize=100&sortorder=distance"
+    url = st.secrets['request_url']
     response = requests.get(url)
     df=pd.DataFrame.empty
     if response.status_code==200:
@@ -30,23 +36,27 @@ def extract_url(hyperlink):
     match = re.search(r'HYPERLINK\("([^"]+)"', hyperlink)
     return match.group(1) if match else None
 
+
 @st.cache_data
 def load_data():
-    data = pd.read_excel('vardcentral.xlsx', engine='openpyxl',engine_kwargs ={'keep_links':True})
+
+    data=pd.read_excel(st.secrets['url'])
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    wb=openpyxl.load_workbook('vardcentral.xlsx')
-    urls=[]
-    for row in  wb['Sheet1'].iter_rows(min_row=2):
-        urls.append(row[16].value)
-    # data['url']=data['url'].apply(lambda x: re.search(r'(https?://\S+)', x).group() if pd.notnull(x) else x)
-    data['url']=urls
-    data['url']=data['url'].apply(extract_url)
+    # wb=openpyxl.load_workbook('vardcentral.xlsx')
+    # urls=[]
+    # for row in  wb['Sheet1'].iter_rows(min_row=2):
+    #     urls.append(row[16].value)
+    # # data['url']=data['url'].apply(lambda x: re.search(r'(https?://\S+)', x).group() if pd.notnull(x) else x)
+    # data['url']=urls
+    # data['url']=data['url'].apply(extract_url)
     return data
 
 data_load_state = st.text('Loading data...')
 data = load_dynamic_data()
 df=load_data()
+df=df[['hsaid','aplicat']]
+res=pd.merge(data,df,on='hsaid')
 data_load_state.text("Done! (using st.cache_data)")
 
 if st.checkbox('Show raw data'):
@@ -55,10 +65,16 @@ if st.checkbox('Show raw data'):
 if st.checkbox('Show excel data'):
     st.subheader('Excel data')
     st.write(df)
-
+optionsInput=res['aplicat'].drop_duplicates().tolist()
+optionsInput.insert(0,'Toate')
+option=st.selectbox('Aplicat',optionsInput)
+if option=='Da':
+    res=res[res['aplicat']=='Da']
+if option=='Nu':
+    res = res[res['aplicat'] == 'Nu']
 center_lat, center_lon = 59.3293, 18.0686
 m=folium.Map(location=[center_lat, center_lon], zoom_start=12)
-for _, row in data.iterrows():
+for _, row in res.iterrows():
     folium.Marker(
         [row['latitude'], row['longitude']],
         popup=f"<a href={row['url']} target=_blank>{row['heading']}<a/> <br>Address: {row['address']}",
